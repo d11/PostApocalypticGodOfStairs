21f0ab605b1f40e52bd2a7f2c98d0ed4acc13dfd
Dan Horgan <danhgn@gmail.com>
2013-03-05 01:34:14 +0000

    Add configuration file and boost asio networking stub code

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -1,11 +1,63 @@
 #include <iostream>
-
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 #include <World.h>
+#include <config.h>
+
+using boost::asio::ip::tcp;
 
 using namespace god;
 
 int main(int argc, char *argv[])
 {
-   std::cout << "Starting client" << std::endl;
+   std::cout << "Starting client (version " << VERSION_STRING << ")" << std::endl;
+   const int port = 16008;
    World world;
+
+   try
+   {
+      if (argc != 2)
+      {
+         std::cerr << "Usage: client <host>" << std::endl;
+         return 1;
+      }
+
+      boost::asio::io_service io_service;
+
+      tcp::resolver resolver(io_service);
+      tcp::resolver::query query(argv[1], std::to_string(port));
+      tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+      tcp::resolver::iterator end;
+
+      tcp::socket socket(io_service);
+      boost::system::error_code error = boost::asio::error::host_not_found;
+      while (error && endpoint_iterator != end)
+      {
+         socket.close();
+         socket.connect(*endpoint_iterator++, error);
+      }
+      if (error)
+         throw boost::system::system_error(error);
+
+      for (;;)
+      {
+         boost::array<char, 128> buf;
+         boost::system::error_code error;
+
+         size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+         if (error == boost::asio::error::eof)
+            break; // Connection closed cleanly by peer.
+         else if (error)
+            throw boost::system::system_error(error); // Some other error.
+
+         std::cout.write(buf.data(), len);
+      }
+   }
+   catch (std::exception& e)
+   {
+      std::cerr << e.what() << std::endl;
+   }
+
+   return 0;
 }
