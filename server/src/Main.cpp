0dcd30ee5f76d579d9c70028fe31aa87eb16c366
Dan Horgan <danhgn@gmail.com>
2013-04-22 01:52:47 +0100

    server: basic structure of network + update code in place for single client

--- server/src/Main.cpp
+++ server/src/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/array.hpp>
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
@@ -32,20 +33,40 @@ class tcp_connection
 
       tcp::socket& socket()
       {
-         return socket_;
+         return _socket;
       }
 
-      void readNetwork()
+      void readNetwork(tcp::socket & socket)
       {
+
+         boost::array<char, 128> buf;
+         boost::system::error_code error;
+         boost::asio::deadline_timer timeout(_io_service);
+         bool data_available = false;
+         size_t len;
+         std::cout << " Set async_read_some " << std::endl;
+         socket.async_read_some(boost::asio::buffer(buf),
+               boost::bind(&tcp_connection::handle_read, this, boost::ref(buf), boost::ref(data_available), boost::ref(timeout), error, len));
+
+
+         std::cout << " Set timer for 800ms " << std::endl;
+         // Clients have a fixed amount of time to make any moves, before the world updates
+         timeout.expires_from_now(boost::posix_time::milliseconds(800));
+         //timeout.async_wait(boost::bind(&wait_callback, boost::ref(socket),
+         //      boost::asio::placeholders::error));
+
+
+         std::cout << " Waiting for timer " << std::endl;
+         timeout.wait();
 
       };
 
-      void writeNetwork()
+      void writeNetwork(tcp::socket & socket)
       {
          std::stringstream s;
-         s << _world.getTime() << ": " << _world.getTestState();
-         message_ = s.str();
-         boost::asio::async_write(socket_, boost::asio::buffer(message_),
+         s << _world.getTime() << ": " << _world.getTestState() << std::endl;
+         _message = s.str();
+         boost::asio::async_write(socket, boost::asio::buffer(_message),
                boost::bind(&tcp_connection::handle_write, shared_from_this(),
                   boost::asio::placeholders::error,
                   boost::asio::placeholders::bytes_transferred));
@@ -55,15 +76,18 @@ class tcp_connection
       {
          for (;;)
          {
-            readNetwork();
+            std::cout << "Reading from clients" << std::endl;
+            readNetwork(_socket);
+            std::cout << "Updating world" << std::endl;
             _world.timeStep();
-            writeNetwork();
+            std::cout << "Writing to clients" << std::endl;
+            writeNetwork(_socket);
          }
       }
 
    private:
       tcp_connection(boost::asio::io_service& io_service, World & world)
-         : socket_(io_service), _world(world)
+         : _io_service(io_service), _socket(io_service), _world(world)
       {
       }
 
@@ -72,8 +96,27 @@ class tcp_connection
       {
       }
 
-      tcp::socket socket_;
-      std::string message_;
+      void handle_read(const boost::array<char, 128> & buf, bool & data_available, boost::asio::deadline_timer& timeout, const boost::system::error_code& error,  size_t bytesRead)
+      {
+         std::cout << "handle_read" << std::endl;
+         if (error == boost::asio::error::eof)
+         {
+            //break; // Connection closed cleanly by peer.
+            return;
+         }
+         else if (error)
+         {
+            throw boost::system::system_error(error); // Some other error.
+         }
+         std::cout << "READ : ";
+         std::cout.write(buf.data(), bytesRead);
+         std::cout << std::endl;
+      }
+
+      boost::asio::io_service & _io_service;
+      tcp::socket _socket;
+      std::string _message;
+      std::stringstream _recieved;
       World & _world;
 };
 
